66915c3e59133c0f3e434843ae10f1ccf198a6b9
dbrcina <dbrcina@donotreply.com>
2022-01-11 12:22:59 +0100

    Set up

--- lab3/microservices/eureka-server/src/main/java/hr/fer/tel/rassus/lab3/EurekaServerApplication.java
+++ lab3/microservices/eureka-server/src/main/java/hr/fer/tel/rassus/lab3/EurekaServerApplication.java
@@ -2,14 +2,14 @@ package hr.fer.tel.rassus.lab3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class TemperatureMicroserviceApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TemperatureMicroserviceApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
