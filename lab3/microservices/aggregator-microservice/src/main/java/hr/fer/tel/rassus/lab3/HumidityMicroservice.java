76f8de5227da90bec07bc1c530f2fae9e0b41f41
dbrcina <dbrcina@donotreply.com>
2022-01-12 13:05:47 +0100

    removed @Lazy annotation

--- lab3/microservices/aggregator-microservice/src/main/java/hr/fer/tel/rassus/lab3/HumidityMicroservice.java
+++ lab3/microservices/aggregator-microservice/src/main/java/hr/fer/tel/rassus/lab3/HumidityMicroservice.java
@@ -1,10 +1,8 @@
 package hr.fer.tel.rassus.lab3;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Lazy
 @FeignClient("${microservices.humidity}")
 public interface HumidityMicroservice {
     @GetMapping("/readings/current")
--- lab3/microservices/aggregator-microservice/src/main/java/hr/fer/tel/rassus/lab3/TemperatureMicroservice.java
+++ lab3/microservices/aggregator-microservice/src/main/java/hr/fer/tel/rassus/lab3/TemperatureMicroservice.java
@@ -1,10 +1,8 @@
 package hr.fer.tel.rassus.lab3;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Lazy
 @FeignClient("${microservices.temperature}")
 public interface TemperatureMicroservice {
     @GetMapping("/readings/current")
